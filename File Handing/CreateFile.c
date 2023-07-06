9705d5b363917805503402a3c4f8a2c5255f0c03
Nanaware Sadanand Santosh <105069814+Sadanand55@users.noreply.github.com>
2023-07-06 17:45:33 +0530

    AddFile

--- File Handing/CreateFile.c
+++ File Handing/CreateFile.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
 
 int main()
 {
-   char *fname[20];
-   int fd=0;
-    
+    char fname[25];
+    int fd=0;
+
+    printf("Enter the file name :");
+    scanf("%s",fname);
+    fd=creat(fname,0777);
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File is created ",fd);
+    }
 }
